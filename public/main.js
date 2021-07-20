d64413ca2deba301498c0af1bd8b83925140a5c4
feguedi <feguedi@yahoo.com.mx>
2021-07-20 18:34:39 -0500

    Update public files

--- public/main.js
+++ public/main.js
@@ -1,35 +1,44 @@
-var Nes = require('nes/client');
-var $ = require('jquery');
-var Handlebars = require('handlebars');
+const Nes = require('@hapi/nes/lib/client');
+const $ = require('jquery');
+const Handlebars = require('handlebars');
 
 //Compile the template
-var source = $("#entry-template").html();
-var template = Handlebars.compile(source);
+const source = $("#entry-template").html();
+const template = Handlebars.compile(source);
 
 //Load initial entries
 $.getJSON('/timeline', function (data) {
 
     data.forEach(function (item) {
 
-        var html = template(item);
+        const html = template(item);
         $("#timeline").append(html);
     });
 });
 
 //Create a new entry
-$('#add-entry').click(function () {
-    $.get('/timeline/createEntry');
+$('#add-entry').on('click', function () {
+    $.post('/timeline/createEntry');
 });
 
 //Setup the websocket connection and react to updates
-var client = new Nes.Client('ws://localhost:3000');
-client.connect(function (err) {
+const client = new Nes.Client('ws://localhost:3000');
 
-    var handler = function (item) {
+async function start () {
+    try {
+        await client.connect();
 
-        var html = template(item);
-        $("#timeline").prepend(html);
-    };
+        const handler = function (item) {
+            console.log('item:', item);
 
-    client.subscribe('/timeline/updates', handler, function (err) {});
-});
+            const html = template(item);
+            $('#timeline').prepend(html);
+        };
+
+        client.subscribe('/timeline/updates', handler);
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+start();
